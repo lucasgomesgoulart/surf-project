3333ff6f485afa752f3354ea7ba5a2b261fea640
lucasgomesgoulart <lucasgoulartg@hotmail.com>
2023-06-27 01:48:33 -0300

    primeiro commit

---
+++ Surf1.0/src/pages/login/index.jsx
@@ -0,0 +1,11 @@
+import './styles.scss'
+
+const Login = () => {
+    return (
+        <div className='container-login'>
+            <h1>Teste</h1>
+        </div>
+    )
+}
+
+export default Login
